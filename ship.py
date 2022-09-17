7aa0bafadc85ceb54d2a6b0fa99557fe88a2b507
AkumuJey <109744817+AkumuJey@users.noreply.github.com>
2022-09-17 23:20:25 +0300

    AlienInvasion Project
    My pygame project

---
+++ ship.py
@@ -0,0 +1,54 @@
+import pygame
+
+
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+    """A class to manage the ship."""
+    def __init__(self, ai_game):
+        """Initializing the ship and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new ship and the bottom of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement flag
+        self.mover_right = False
+        self.move_left = False
+        self.move_up = False
+        self.move_down = False
+
+    def update(self):
+        """Update ship position based on flag"""
+        if self.mover_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.move_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        elif self.move_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        elif self.move_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the sheep at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship at the bottom of the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
